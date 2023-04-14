4a9f507cf6c414fa450148ec1a26e1b98e267843
betonucon <betonucon@gmail.com>
2023-04-14 13:28:19 +0700

    push

--- resources/views/kasir/cetak.blade.php
+++ resources/views/kasir/cetak.blade.php
@@ -1,6 +1,6 @@
 <html>
     <head>
-        <title>CETAK</title>
+        <title>CETAK NOTA</title>
         <style>
             html{
                 color:#000;
--- resources/views/layouts/web.blade.php
+++ resources/views/layouts/web.blade.php
@@ -3,7 +3,7 @@
 <html lang="en">
 <head>
 	<meta charset="utf-8" />
-	<title>DG WARUNG</title>
+	<title>BP SUPPLIER</title>
 	<meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" name="viewport" />
 	<meta content="" name="description" />
 	<meta content="" name="author" />
